95aadd328878d5cbb75fdd43900172c36a8c08d5
Dragan Milic <dragan@netice9.com>
2015-05-10 03:01:37 +0100

    refactores application.js to use container

--- application.js
+++ application.js
@@ -1,12 +1,14 @@
 'use strict';
 
 var docker = require('./docker');
-var Stately = require('stately.js');
 var async = require('async');
 var _ = require('lodash');
 var DepGraph = require('dependency-graph').DepGraph;
 
 var dateFormat = require('dateformat');
+var util = require('util');
+var events = require("events");
+var container = require("./container");
 
 function Logger() {
 
@@ -22,11 +24,8 @@ function Logger() {
   this.error = this.log.bind(this,"error");
 }
 
+
 function Application(config) {
-
-
-  var images = _.values(config.containers).map(function(container) { return [container.image, container.authconfig]; });
-
 
   var that = this;
 
@@ -34,189 +33,45 @@ function Application(config) {
 
   this.containers = {};
 
-  this.machine = Stately.machine({
-
-    INITIAL: {
-      start: function() {
-        return this.FETCHING_IMAGES;
-      }
-    },
-
-    FETCHING_IMAGES: {
-    },
-
-    CREATING_CONTAINERS: {
-    },
-
-    STARTING_CONTAINERS: {
-
-    },
-
-    RUNNING: {
-
-    },
-
-    SHUTTING_DOWN: {
-
-    },
-
-    TERMINATED: {
-
-    },
-
-    ERROR: {
-
-    }
-
-
-  });
-
-
-  this.machine.onFETCHING_IMAGES = function() {
-
-    that.logger.info("fetching images");
-
-    async.each(images, function(imageAuth, callback) {
-      that.logger.info("pulling "+imageAuth[0]);
-
-      docker.pull(imageAuth[0], {authconfig: imageAuth[1]}, function(err, stream) {
-        if (err) {
-          return callback(err);
-        } else {
-          var onProgress = function(event) {
-            //...
-          }.bind(this);
-
-          docker.modem.followProgress(stream, callback, onProgress);
-      }
-
-      });
-    }, function(err, stream) {
-      if (err) {
-        that.logger.error(err);
-        return this.setMachineState(this.SHUTTING_DOWN);
-      } else {
-        this.setMachineState(this.CREATING_CONTAINERS);
-      }
-    }.bind(this));
-
-    return this.FETCHING_IMAGES;
+  var prefixApplicationName = function(name) {
+    return config.name + "." + name;
   };
 
-  this.machine.onCREATING_CONTAINERS = function() {
-    that.logger.info("creating containers");
-
-    var graph = new DepGraph();
-
-    _.keys(config.containers).forEach(function(containerName){ graph.addNode(containerName); });
-
-    _.pairs(config.containers).forEach(function(name, container) {
-      var deps = (container.links || []).map(function(link){return link.split(/:/)[0]});
-
-      deps.forEach(function(dep) { graph.addDependency(name, dep); });
-
-    });
-
-    that.logger.info("start order: "+JSON.stringify(graph.overallOrder()));
-
-    async.mapSeries(graph.overallOrder(), function(containerToStart, callback) {
-      var container = config.containers[containerToStart];
-      var containerName = config.name + "." + containerToStart;
-
-      var links = (container.links || []).map(function(link){ return config.name + "." + link});
+  _.pairs(config.containers).forEach(function(kv) {
+    var containerName = prefixApplicationName(kv[0]);
+    var containerConfig = JSON.parse(JSON.stringify(kv[1]));
+    containerConfig.volumes_from = (containerConfig.volumes_from || []).map(prefixApplicationName);
+    containerConfig.links = (containerConfig.links || []).map(prefixApplicationName);
+    var fileName = containerName + ".id";
+    this.containers[kv[0]]=new container.Container(containerName, containerConfig, fileName);
+  }.bind(this));
 
 
-      var environment = _.pairs(container.environment || {}).map(function(pair){return pair.join('=');});
+  async.series([
+    function(cb) {
+      async.each(_.values(this.containers), function(container, cb2) {
+        container.fetchImage(cb2);
+      }, cb);
+    }.bind(this),
+    function(cb) {
+      var graph = new DepGraph();
+      _.keys(config.containers).forEach(function(containerName){ graph.addNode(containerName); });
 
-      var volumesFrom = (container.volumes_from || []).map(function(v) {return config.name+"."+v});
-
-      var portBindings = {};
-
-      _.pairs(container.tcp_port_bindings || {}).forEach(function(pair) {
-        var key =  pair[0]+"/tcp"
-        var value = [{"HostPort": pair[1]}]
-        portBindings[key] = value;
+      _.pairs(config.containers).forEach(function(name, container) {
+          var deps = (container.links || []).map(function(link){return link.split(/:/)[0]});
+          deps.forEach(function(dep) { graph.addDependency(name, dep); });
       });
 
-      _.pairs(container.udp_port_bindings || {}).forEach(function(pair) {
-        var key =  pair[0]+"/udp"
-        var value = [{"HostPort": pair[1]}]
-        portBindings[key] = value;
-      });
+      var overallOrder = graph.overallOrder();
 
-      var createContainerConfig = {
-        name: containerName,
-        'Hostname': '',
-        'User': '',
-        'AttachStdin': false,
-        'AttachStdout': true,
-        'AttachStderr': true,
-        'Tty': true,
-        'OpenStdin': false,
-        'StdinOnce': false,
-        'Env': environment,
-        'Cmd': container.command,
-        'Image': container.image,
-        'Volumes': {},
+      // that.logger.info("start order: "+JSON.stringify(overallOrder));
+      console.log("start order: "+JSON.stringify(overallOrder));
 
-        'HostConfig': {
-          'PortBindings': portBindings,
-          'Links': links,
-          'VolumesFrom': volumesFrom,
-        }
-      };
-      that.logger.info("createOptions options: " + JSON.stringify(createContainerConfig));
-      docker.createContainer(createContainerConfig, function(err, container) {
-        if (!err) {
-          that.containers[containerName] = container;
-        }
-        callback(err, container);
-      });
-    }.bind(this), function(err, containers) {
-      if (err) {
-        that.logger.error(err);
-        return this.setMachineState(this.SHUTTING_DOWN);
-      } else {
-        return this.setMachineState(this.STARTING_CONTAINERS);
-      }
-    }.bind(this));
-
-  };
-
-  this.machine.onSTARTING_CONTAINERS = function() {
-    that.logger.info("starting containers");
-    async.eachSeries(_.values(that.containers), function(container, callback) {
-      that.logger.info("starting container " + container.id);
-      container.start(callback);
-    }, function(err) {
-      if (err) {
-        that.logger.error(err);
-        return this.setMachineState(this.SHUTTING_DOWN);
-      } else {
-        return this.setMachineState(this.RUNNING);
-      }
-    }.bind(this));
-  };
-
-  this.machine.onRUNNING = function() {
-    that.logger.info("containers are running");
-  }
-
-  this.machine.onSHUTTING_DOWN = function() {
-    that.logger.info("shutting down")
-    async.each(_.values(that.containers), function(container, callback) {
-      container.remove({force: true}, callback);
-    }.bind(this), function(err) {
-      if (err) {
-        return this.setMachineState(this.ERROR);
-      }
-      that.containers = [];
-      this.setMachineState(this.TERMINATED);
-    }.bind(this));
-  }
-
-  this.machine.start();
-
+      async.eachSeries(overallOrder, function(containerName, cb2) {
+        this.containers[containerName].start(cb2);
+      }.bind(this), cb);
+    }.bind(this)
+  ]);
 }
 
 
--- container.js
+++ container.js
@@ -5,13 +5,15 @@ var async = require('async');
 var _ = require('lodash');
 var util = require('util');
 var events = require("events");
+var fs = require('fs');
 
-function Container(name, config) {
+function Container(name, config, fileName) {
 
   events.EventEmitter.call(this);
 
   this.name = name;
   this.config = config;
+  this.fileName = fileName;
 
   this.fetchImage = function(callback) {
 
@@ -19,85 +21,58 @@ function Container(name, config) {
       callback = function(){};
     }
 
-    docker.pull(config.image, {authconfig: config.authconfig}, function(err, stream) {
-      if (err) {
-        this.emit('failure', 'fetchImage', this.name, err);
-        callback(err);
-      } else {
-        docker.modem.followProgress(stream, function(err) {
+    async.series([
+      function(cb) {
+        fs.readFile(this.fileName, function(err, data) {
           if (err) {
-            this.emit('failure', 'fetchImage', this.name, err);
-            callback(err);
+            this.containerId = null;
+            console.log("no container id");
           } else {
-            this.emit('imageFetched',this.name);
-            callback();
+            this.containerId = data;
           }
-        }.bind(this), function(event) {
-        });
-      }
-    }.bind(this));
-
+          cb();
+        }.bind(this));
+      }.bind(this),
+      function(cb) {
+        if (this.containerId) {
+          var container = docker.getContainer(this.containerId);
+          container.inspect(function(err, data) {
+            if (err) {
+              this.containerId = null;
+            }
+            cb();
+          }.bind(this));
+        } else {
+          cb();
+        }
+      }.bind(this),
+      function(cb) {
+        if (this.containerId) {
+          cb();
+        } else {
+          console.log("pulling");
+          docker.pull(config.image, {authconfig: config.authconfig}, function(err, stream) {
+            if (err) {
+              this.emit('failure', 'fetchImage', this.name, err);
+              cb(err);
+            } else {
+              docker.modem.followProgress(stream, function(err) {
+                if (err) {
+                  this.emit('failure', 'fetchImage', this.name, err);
+                  cb(err);
+                } else {
+                  this.emit('imageFetched',this.name);
+                  cb();
+                }
+              }.bind(this), function(event) {
+              });
+            }
+          }.bind(this));
+        }
+      }.bind(this)
+    ], callback);
   }
 
-  this.createDockerContainer = function(callback) {
-
-    if (!callback) {
-      callback = function(){};
-    }
-
-    var links = (config.links || []);
-
-    var environment = _.pairs(config.environment || {}).map(function(pair){return pair.join('=');});
-
-    var volumesFrom = (config.volumes_from || []);
-
-    var portBindings = {};
-
-    _.pairs(config.tcp_port_bindings || {}).forEach(function(pair) {
-      var key =  pair[0]+"/tcp"
-      var value = [{"HostPort": pair[1]}]
-      portBindings[key] = value;
-    });
-
-    _.pairs(config.udp_port_bindings || {}).forEach(function(pair) {
-      var key =  pair[0]+"/udp"
-      var value = [{"HostPort": pair[1]}]
-      portBindings[key] = value;
-    });
-
-    var createContainerConfig = {
-      name: name,
-      'Hostname': '',
-      'User': '',
-      'AttachStdin': false,
-      'AttachStdout': true,
-      'AttachStderr': true,
-      'Tty': true,
-      'OpenStdin': false,
-      'StdinOnce': false,
-      'Env': environment,
-      'Cmd': config.command,
-      'Image': config.image,
-      'Volumes': {},
-
-      'HostConfig': {
-        'PortBindings': portBindings,
-        'Links': links,
-        'VolumesFrom': volumesFrom,
-      }
-    };
-
-    docker.createContainer(createContainerConfig, function(err, container) {
-      if (err) {
-        this.emit('failure', 'createDockerContainer',this.name, err);
-        callback(err);
-      } else {
-        this.container = container;
-        this.emit('dockerContainerCreated',this.name);
-        callback();
-      }
-    }.bind(this));
-  };
 
   this.start = function(callback) {
 
@@ -105,23 +80,101 @@ function Container(name, config) {
       callback = function(){};
     }
 
-    this.container.start(function(err) {
-      if (err) {
-        this.emit('failure', 'startContainer', this.name, err);
-        callback(err);
-      } else {
-        this.emit('started',this.name);
-        callback();
-      }
-    }.bind(this));
+
+    async.series([
+      function(cb) {
+
+        if (!this.containerId) {
+
+          var links = (config.links || []);
+
+          var environment = _.pairs(config.environment || {}).map(function(pair){return pair.join('=');});
+
+          var volumesFrom = (config.volumes_from || []);
+
+          var portBindings = {};
+
+          _.pairs(config.tcp_port_bindings || {}).forEach(function(pair) {
+            var key =  pair[0]+"/tcp"
+            var value = [{"HostPort": pair[1]}]
+            portBindings[key] = value;
+          });
+
+          _.pairs(config.udp_port_bindings || {}).forEach(function(pair) {
+            var key =  pair[0]+"/udp"
+            var value = [{"HostPort": pair[1]}]
+            portBindings[key] = value;
+          });
+
+          var createContainerConfig = {
+            name: name,
+            'Hostname': '',
+            'User': '',
+            'AttachStdin': false,
+            'AttachStdout': true,
+            'AttachStderr': true,
+            'Tty': true,
+            'OpenStdin': false,
+            'StdinOnce': false,
+            'Env': environment,
+            'Cmd': config.command,
+            'Image': config.image,
+            'Volumes': {},
+
+            'HostConfig': {
+              'PortBindings': portBindings,
+              'Links': links,
+              'VolumesFrom': volumesFrom,
+            }
+          };
+
+          docker.createContainer(createContainerConfig, function(err, container) {
+            if (err) {
+              this.emit('failure', 'createDockerContainer',this.name, err);
+              return cb(err);
+            } else {
+              this.containerId = container.id;
+              fs.writeFileSync(this.fileName,container.id);
+              this.emit('dockerContainerCreated',this.name);
+              return cb();
+            }
+          }.bind(this)
+          );
+        } else {
+          cb();
+        }
+      }.bind(this),
+      function(cb) {
+        var container = docker.getContainer(this.containerId);
+
+        container.inspect(function(err, data) {
+          if (err) {
+            return cb(err);
+          } else {
+
+            if (!data.State.Running) {
+              container.start(function(err) {
+                if (err) {
+                  this.emit('failure', 'startContainer', this.name, err);
+                  return cb(err);
+                } else {
+                  this.emit('started',this.name);
+                  return cb();
+                }
+              }.bind(this))
+
+            } else {
+              return cb();
+            }
+          }
+        }.bind(this));
+      }.bind(this)
+    ], callback);
   }
 
 }
 
-
 util.inherits(Container, events.EventEmitter);
-
-
 
 module.exports = {
   Container: Container
